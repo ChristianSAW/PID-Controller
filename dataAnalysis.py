c29945a309441663eca363b5430686df2e545cec
Christian Welling <csw73@cornell.edu>
2019-04-26 00:33:32 -0700

    Added more plots to data analysis

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,10 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-#text_file = open("output_files/datalog_1.txt", "r")
-#lines = text_file.read().split(' ')
-
-print("Testing things")
+print("Begin Analysis")
 
 # Make Data Structure
 class DataLog:
@@ -28,15 +25,58 @@ with open("output_files/datalog_1.txt", "r") as f:
         dl1.cte.append(float(row[4]))
 
 
+# PLOTTING
+
+figX = 16
+figY = 8
+
 y_cte = np.array(dl1.cte)
-#t = np.linspace(0, len(y_cte)-1, len(y_cte))
+y_speed = np.array(dl1.speed)
+y_throttle = np.array(dl1.throttle)
+y_steer = np.array(dl1.steer_value)
+y_angle = np.array(dl1.angle)
+t = np.linspace(0, len(y_cte)-1, len(y_cte))
 
-#for i in range(len(y_cte)):
-#    print(y_cte[i])
 
-
-#plt.plot(t, dl1.cte)
+# Plot CTE
+f0 = plt.figure(1)
 plt.plot(y_cte)
 plt.ylabel('CTE')
-plt.show()
+f0.show()
 
+# PLOT CTE, Steer_Value, Angle
+f1, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(figX, figY))
+f1.tight_layout()
+
+ax1.plot(y_cte)
+ax1.set_title('CTE')
+
+ax2.plot(y_steer)
+ax2.set_title('Steering Value')
+
+ax3.plot(y_angle)
+ax3.set_title('Angle')
+
+f1.show()
+
+# Plote CTE, Throttle, Speed
+
+f2, (ax4, ax5, ax6) = plt.subplots(3, 1, figsize=(figX, figY))
+f2.tight_layout()
+
+ax4.plot(y_cte)
+ax4.set_title('CTE')
+#ax4.show()
+
+ax5.plot(y_throttle)
+ax5.set_title('Throttle')
+#ax5.show()
+
+ax6.plot(y_speed)
+ax6.set_title('Speed')
+#ax6.show()
+
+f2.show()
+
+# must call this line to keep figs alive
+input()
